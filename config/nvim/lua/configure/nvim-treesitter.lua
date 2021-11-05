2f1b5a0f138fa7d307a96650254a9d84e087c0a7
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-11-05 13:15:38 -0400

    Update colors for rainbow to work w/ nord theme.

--- config/nvim/lua/configure/nvim-treesitter.lua
+++ config/nvim/lua/configure/nvim-treesitter.lua
@@ -17,6 +17,15 @@ require("nvim-treesitter.configs").setup {
   rainbow = {
     enable = true,
     extended_mode = true,
-    max_file_lines = 1500
+    max_file_lines = 1500,
+    colors = {
+      "#bf616a",
+      "#d08770",
+      "#ebcb8b",
+      "#a3be8c",
+      "#88c0d0",
+      "#5e81ac",
+      "#b48ead",
+    },
   }
 }
