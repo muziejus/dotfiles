d3a6c7b11771d2716b2a08591ea793ba21f668ec
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-12-07 18:13:21 -0500

    fight with vimwiki.

--- config/nvim/lua/configure/vimwiki.lua
+++ config/nvim/lua/configure/vimwiki.lua
@@ -1,10 +1,24 @@
 return {
   'vimwiki/vimwiki',
   config = function() 
+    -- vim.cmd([[
+    --   let g:vimwiki_list=[{'path': '/Users/moacir/Dropbox/notes',
+    --   \ 'syntax': 'markdown', 'ext': '.wikimd'}]
+    -- ]])
     vim.g.vimwiki_list = {{
       path = '/Users/moacir/Dropbox/notes', 
       syntax = 'markdown', 
-      ext = '.md'}}
+      ext = '.wikimd'}} -- This will hopefully stop treating every md file as a vimwiki file.
+    vim.g.vimwiki_global_ext = 0
+    vim.cmd([[
+    let g:vimwiki_ext2syntax = {}
+    ]])
+    -- vim.cmd([[
+    -- let g:vimwiki_ext2syntax = {'.wikimd': 'markdown'}
+    -- ]])
+    -- vim.cmd([[
+    --   let g:vimwiki_global_ext = 0
+    -- ]])
   end
 }
 
