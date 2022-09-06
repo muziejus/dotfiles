4b3df04df14985f6440c3b732ecfc1badf7b60d9
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-09-06 10:23:56 -0400

    Restoring minimal config so cmp works.

--- config/nvim/lua/configure/completion.lua
+++ config/nvim/lua/configure/completion.lua
@@ -1,20 +1,21 @@
 return {
   "hrsh7th/nvim-cmp", -- The completion plugin
   requires = {
+    "nvim-lua/plenary.nvim", -- needed downstream
     "hrsh7th/cmp-nvim-lsp",
-    -- "hrsh7th/cmp-buffer", -- buffer completions
+    "hrsh7th/cmp-buffer", -- buffer completions
     "hrsh7th/cmp-path", -- path completions
     "hrsh7th/cmp-cmdline", -- cmdline completions
-    "hrsh7th/cmp-nvim-lua", -- for vim config files.
-    "onsails/lspkind-nvim",
+    --"hrsh7th/cmp-nvim-lua", -- for vim config files.
+    --"onsails/lspkind-nvim",
     -- compare: https://github.com/aspeddro/cmp-pandoc.nvim
     "aspeddro/cmp-pandoc.nvim",
     -- "jc-doyle/cmp-pandoc-references", -- pandoc completions
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip", -- snippet completions
+    "hrsh7th/cmp-vsnip",
+    "hrsh7th/vim-vsnip",
     "rafamadriz/friendly-snippets", -- snippet collection
-    "petertriho/cmp-git",
-    "davidsierradz/cmp-conventionalcommits",
+    --"petertriho/cmp-git",
+    --"davidsierradz/cmp-conventionalcommits",
     "uga-rosa/cmp-dictionary",
   },
   -- after = { 'nvim-autopairs' },
@@ -24,95 +25,92 @@ return {
       return
     end
 
-    local snip_status_ok, luasnip = pcall(require, "luasnip")
-    if not snip_status_ok then
-      return
-    end
-
-    require("luasnip/loaders/from_vscode").lazy_load() -- ?
-
     local check_backspace = function()
       local col = vim.fn.col(".") - 1
       return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
     end
 
     cmp.setup({
-      enabled = true,
-      completion = {
-        autocomplete = false,
-      },
+--      completion = {
+--        autocomplete = true,
+--      },
       snippet = {
         -- A snippet engine MUST be defined
         expand = function(args)
-          luasnip.lsp_expand(args.body)
+          vim.fn["vsnip#anonymous"](args.body)
         end,
       },
       window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
       },
-      mapping = {
-        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ["<C-e>"] = cmp.mapping({
-          i = cmp.mapping.abort(),
-          c = cmp.mapping.close(),
-        }),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif lusasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, {
-          "i",
-          "s",
-        }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expandable() then
-            luasnip.expand()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif check_backspace() then
-            fallback()
-          else
-            fallback()
-          end
-        end, {
-          "i",
-          "s",
-        }),
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+--         ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+--         ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+--         ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+--         ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+--         ["<C-e>"] = cmp.mapping({
+--           i = cmp.mapping.abort(),
+--           c = cmp.mapping.close(),
+--         }),
+--         ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+--         ["<S-Tab>"] = cmp.mapping(function(fallback)
+--           if cmp.visible() then
+--             cmp.select_prev_item()
+--           elseif lusasnip.jumpable(-1) then
+--             luasnip.jump(-1)
+--           else
+--             fallback()
+--           end
+--         end, {
+--           "i",
+--           "s",
+--         }),
+--         ["<Tab>"] = cmp.mapping(function(fallback)
+--           if cmp.visible() then
+--             cmp.select_next_item()
+--           elseif luasnip.expandable() then
+--             luasnip.expand()
+--           elseif luasnip.expand_or_jumpable() then
+--             luasnip.expand_or_jump()
+--           elseif check_backspace() then
+--             fallback()
+--           else
+--             fallback()
+--           end
+--         end, {
+--           "i",
+--           "s",
+--         }),
+--         ["<C-p>"] = cmp.mapping.select_prev_item(),
+--         ["<C-n>"] = cmp.mapping.select_next_item(),
+      }),
       sources = cmp.config.sources({
         -- Order matters.
-        { name = "luasnip" },
         { name = "nvim_lsp" },
-        -- { name = "buffer" },
+        { name = "vsnip" },
         -- { name = "path" },
         -- { name = 'treesitter' },
-        -- { name = "pandoc_references" },
         -- { name = 'spell' },
-      }),
-      confirm_opts = {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = false,
-      },
-      formatting = {
-        format = require("lspkind").cmp_format({ with_text = true }),
-      },
-      experimental = {
-        ghost_text = true,
-        native_menu = false, -- See below
-      },
+      }, {
+        { name = "buffer" },
+      })
+--       confirm_opts = {
+--         behavior = cmp.ConfirmBehavior.Replace,
+--         select = false,
+--       },
+--       formatting = {
+--         format = require("lspkind").cmp_format({ with_text = true }),
+--       },
+--       experimental = {
+--         ghost_text = true,
+--         native_menu = false, -- See below
+--       },
     })
 
     require("cmp_pandoc").setup()
@@ -123,16 +121,20 @@ return {
       },
     })
 
+    print("got past pandoc and dictionary setup")
+
     cmp.setup.filetype("markdown", {
       sources = cmp.config.sources({
         { name = "cmp_pandoc" },
         { name = "dictionary", keyword_length = 2 },
+      }, {
+        {name="buffer"}
       }),
     })
 
     cmp.setup.filetype("lua", {
       sources = cmp.config.sources({
-        { name = "luasnip" },
+        { name = "vsnip" },
         { name = "nvim_lsp" },
         { name = "nvim_lua" },
       }),
@@ -140,7 +142,7 @@ return {
 
     cmp.setup.filetype({ "javscript", "typescript" }, {
       sources = cmp.config.sources({
-        { name = "luasnip" },
+        { name = "vsnip" },
         { name = "nvim_lsp" },
       }),
     })
@@ -154,15 +156,13 @@ return {
       }),
     })
 
-    -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-    -- cmp.setup.cmdline("/", {
-    --   mapping = cmp.mapping.preset.cmdline(),
-    --   sources = {
-    --     { name = "buffer" },
-    --   },
-    -- })
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
 
-    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline(":", {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -46,50 +46,50 @@ return packer.startup(function(use)
   use(require("configure.kitty-navigator"))
   use(require("configure.which-key"))
   -- Autocomplete, syntax, and LSP
-  use(require("configure.lspconfig"))
+  -- use(require("configure.lspconfig"))
   use(require("configure.completion"))
-  use(require("configure.treesitter"))
-  use(require("configure.trouble"))
-  use(require("configure.typescript"))
-  -- use(require("configure.lsp-signature")) -- conflicts with nvim-cmp
-  -- Look and Feel
-  use(require("configure/auto-dark-mode"))
-  use("sainnhe/everforest")
-  use("fladson/vim-kitty")
-  use("tpope/vim-commentary") -- creates gcc for commenting things
-  use(require("configure.web-devicons"))
-  use(require("configure.lualine"))
-  use(require("configure.tabline"))
-  use(require("configure.indent-blankline"))
-  -- Utilities / Dependencies
-  use(require("configure.telescope"))
-  use(require("configure.telescope-fzf-native"))
+  -- use(require("configure.treesitter"))
+  -- use(require("configure.trouble"))
+  -- use(require("configure.typescript"))
+  -- -- use(require("configure.lsp-signature")) -- conflicts with nvim-cmp
+  -- -- Look and Feel
+  -- use(require("configure/auto-dark-mode"))
+  -- use("sainnhe/everforest")
+  -- use("fladson/vim-kitty")
+  -- use("tpope/vim-commentary") -- creates gcc for commenting things
+  -- use(require("configure.web-devicons"))
+  -- use(require("configure.lualine"))
+  -- use(require("configure.tabline"))
+  -- use(require("configure.indent-blankline"))
+  -- -- Utilities / Dependencies
+  -- use(require("configure.telescope"))
+  -- use(require("configure.telescope-fzf-native"))
   use(require("configure.toggleterm"))
-  use("nvim-lua/plenary.nvim")
-  -- Org
-  use(require("configure.wiki"))
-  -- Editing
-  use(require("configure.zen-mode"))
-  use(require("configure.twilight"))
-  use("thinca/vim-visualstar")
-  use("tpope/vim-surround")
-  use("tpope/vim-unimpaired") -- [ and ] macros for moving around
-  use(require("configure.autopairs"))
-  -- Git
-  use(require("configure.gitsigns"))
-  use("tpope/vim-fugitive") -- Git wrapper.
-  use("tpope/vim-rhubarb") -- GitHub
-  -- Filesystem
-  use("tpope/vim-eunuch") -- Work on filesystem via buffers
-  use("tpope/vim-projectionist")
-  use(require("configure.nvim-tree"))
-  use("chazy/dirsettings") -- for autocommitting wiki.
-  -- Text
-  use("kana/vim-textobj-user") -- define textobjs easily
-  use("reedes/vim-textobj-quote") -- for curly quotes
-  use("reedes/vim-textobj-sentence") -- for sentences
-  use(require("configure.pandoc"))
-  -- use("vim-pandoc/vim-pandoc-syntax")
+  -- use("nvim-lua/plenary.nvim")
+  -- -- Org
+  -- use(require("configure.wiki"))
+  -- -- Editing
+  -- use(require("configure.zen-mode"))
+  -- use(require("configure.twilight"))
+  -- use("thinca/vim-visualstar")
+  -- use("tpope/vim-surround")
+  -- use("tpope/vim-unimpaired") -- [ and ] macros for moving around
+  -- use(require("configure.autopairs"))
+  -- -- Git
+  -- use(require("configure.gitsigns"))
+  -- use("tpope/vim-fugitive") -- Git wrapper.
+  -- use("tpope/vim-rhubarb") -- GitHub
+  -- -- Filesystem
+  -- use("tpope/vim-eunuch") -- Work on filesystem via buffers
+  -- use("tpope/vim-projectionist")
+  -- use(require("configure.nvim-tree"))
+  -- use("chazy/dirsettings") -- for autocommitting wiki.
+  -- -- Text
+  -- use("kana/vim-textobj-user") -- define textobjs easily
+  -- use("reedes/vim-textobj-quote") -- for curly quotes
+  -- use("reedes/vim-textobj-sentence") -- for sentences
+  -- use(require("configure.pandoc"))
+  -- -- use("vim-pandoc/vim-pandoc-syntax")
 
   if PACKER_BOOTSTRAP then
     require("packer").sync()
