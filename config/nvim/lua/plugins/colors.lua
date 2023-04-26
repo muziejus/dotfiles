0c6908f09b0f86f5f182d65fbcb90044e592281a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-04-26 13:26:08 -0400

    feat: Change colorscheme

--- config/nvim/lua/plugins/colors.lua
+++ config/nvim/lua/plugins/colors.lua
@@ -1,8 +1,6 @@
 return {
-  {"rose-pine/neovim",
-    name = 'rose-pine',
-    config = function()
-        vim.cmd('colorscheme rose-pine')
-    end
-  }
+  -- {"rose-pine/neovim",
+  --   name = 'rose-pine',
+  -- },
+  "sainnhe/everforest",
 }
