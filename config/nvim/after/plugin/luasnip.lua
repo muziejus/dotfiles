b4071a30f48e1aa3c5835179cb7de27dbec58a68
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-05-26 09:19:30 -0400

    feat: Start adding snippets.

---
+++ config/nvim/after/plugin/luasnip.lua
@@ -0,0 +1,16 @@
+local ls = require("luasnip")
+
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets" })
+
+ls.config.set_config({
+  history = true,
+  updateevents = "TextChanged,TextChangedI",
+  enable_autosnippets = true,
+  -- ext_opts = {
+  --   [require("luasnip.util.types").choiceNode] = {
+  --     active = {
+  --       virt_text = { {"•", "GruvboxOrange" } },
+  --     },
+  --   },
+  -- },
+})
