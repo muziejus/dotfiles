88a41504de07037f4ae1c464fc95e2eb70bbd188
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-06-23 09:26:15 -0400

    feat: Put skim setting in init for VimTeX.

--- config/nvim/lua/plugins/text.lua
+++ config/nvim/lua/plugins/text.lua
@@ -1,7 +1,10 @@
-vim.g.vimtex_view_method = "skim"
-
 return {
-  {"lervag/vimtex"},
+  {
+    "lervag/vimtex",
+    init = function()
+      vim.g.vimtex_view_method = "skim"
+    end,
+  },
   {"jbyuki/nabla.nvim"},
   {"vim-pandoc/vim-pandoc"},
   {"vim-pandoc/vim-pandoc-syntax"},
