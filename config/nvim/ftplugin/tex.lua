f5b0341eee7a4c9339c27f2ba92ca6cb374826b6
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-06-28 06:06:57 -0400

    feat: Add surrounds and new conceal color in vimtex

--- config/nvim/ftplugin/tex.lua
+++ config/nvim/ftplugin/tex.lua
@@ -1,10 +1,12 @@
+local colors = require("tokyonight.colors").setup()
+
 vim.g.vimtex_delim_toggle_mod_list = {
-  {"\\left", "\\right"},
-  {"\\big", "\\big"},
+	{ "\\left", "\\right" },
+	{ "\\big", "\\big" },
 }
 
 vim.g.vimtex_compiler_latexmk_engines = {
-  ["_"] = "-xelatex",
+	["_"] = "-xelatex",
 }
 
 -- vim.g.vimtex_view_method = "skim" -- this is set in init.lua
@@ -13,3 +15,29 @@ vim.g.vimtex_view_skim_activate = 1
 vim.g.vimtex_view_skim_reading_bar = 1
 
 vim.opt["conceallevel"] = 2
+vim.cmd("highlight Conceal guifg=" .. colors.orange .. " guibg=NONE")
+
+require("nvim-surround").buffer_setup({
+	surrounds = {
+		["Q"] = {
+			add = { "``", "''" },
+			find = "%b``.-''",
+			delete = "^(``)().-('')()$",
+		},
+		["q"] = {
+			add = { "`", "'" },
+			find = "`.-'",
+			delete = "^(`)().-(')()$",
+		},
+		["B"] = {
+			add = { "\\textbf{", "}" },
+			find = "\\%a-bf%b{}",
+			delete = "^(\\%a-bf{)().-(})()$",
+		},
+		["I"] = {
+			add = { "\\textit{", "}" },
+			find = "\\%a-it%b{}",
+			delete = "^(\\%a-it{)().-(})()$",
+		},
+	},
+})
