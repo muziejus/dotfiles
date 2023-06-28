bee475fe28ddf61dc9908857cb3573115e05b793
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-06-28 11:25:30 -0400

    feat: Comment out default telescope bibtex config

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -4,22 +4,23 @@ local telescope = require("telescope")
 
 telescope.load_extension("bibtex")
 
-telescope.setup {
-  defaults = {
-    mappings = {
-      i = { ["<c-t>"] = trouble.open_with_trouble },
-      n = { ["<c-t>"] = trouble.open_with_trouble },
-    },
-  },
-  load_extensions = { "yank_history", "bibtex" },
-  extensions = {
-    bibtex = {
-      depth = 1,
-      global_files = {"~/Library/texmf/bibtex/bib/Zotero.bib"},
-      search_keys = { "author", "year", "title" },
-      citation_format = "{{author}} ({{year}}) {{title}}",
-      citation_trim_firstname = true,
-      citation_max_auth = 2,
-    },
-  },
-}
+telescope.setup({
+	defaults = {
+		mappings = {
+			i = { ["<c-t>"] = trouble.open_with_trouble },
+			n = { ["<c-t>"] = trouble.open_with_trouble },
+		},
+	},
+	load_extensions = { "yank_history", "bibtex" },
+	extensions = {
+		bibtex = {
+			-- Defaults commented out
+			-- depth = 1,
+			global_files = { "/Users/moacir/Library/texmf/bibtex/bib/Zotero.bib" },
+			-- search_keys = { "author", "year", "title" },
+			-- citation_format = "{{author}} ({{year}}) {{title}}",
+			-- citation_trim_firstname = true,
+			-- citation_max_auth = 2,
+		},
+	},
+})
