cd28c5a7291fee2d63aa202124a29913c177d0aa
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-07-13 11:57:05 -0400

    feat: use HOMEBREW_PREFIX to find jdtls.

--- config/nvim/ftplugin/java.lua
+++ config/nvim/ftplugin/java.lua
@@ -1,6 +1,8 @@
+local prefix = os.getenv("HOMEBREW_PREFIX")
+
 local config = {
-  cmd = {"/usr/local/bin/jdtls"},
-  root_dir = vim.fs.dirname(vim.fs.find({"gradlew", ".git", "mvnw"}, { upward = true})[1]),
+	cmd = { prefix .. "/bin/jdtls" },
+	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
 }
 
 require("jdtls").start_or_attach(config)
