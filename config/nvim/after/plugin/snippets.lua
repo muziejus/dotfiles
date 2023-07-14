da379d58562d4cb8cdd6f75f22c371b4c0b27e56
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-07-14 17:51:14 -0400

    feat: Start faffing about w/ highlighting and virtual text of snippets.

--- config/nvim/after/plugin/snippets.lua
+++ config/nvim/after/plugin/snippets.lua
@@ -1,19 +1,30 @@
 local ls = require("luasnip")
+local types = require("luasnip.util.types")
 
-require('luasnip.loaders.from_vscode').lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets" })
 
 ls.config.setup({
-  history = true,
-  updateevents = "TextChanged,TextChangedI",
-  enable_autosnippets = true,
-  -- ext_opts = {
-  --   [require("luasnip.util.types").choiceNode] = {
-  --     active = {
-  --       virt_text = { {"•", "GruvboxOrange" } },
-  --     },
-  --   },
-  -- },
+	history = true,
+	updateevents = "TextChanged,TextChangedI",
+	enable_autosnippets = true,
+	ext_opts = {
+		[types.insertNode] = {
+			active = {
+				hl_group = "TermCursor",
+			},
+			unvisited = {
+				virt_text = { { "unvisited", "Search" } },
+			},
+			passive = {
+				virt_text = { { "Pass", "Search" } },
+			},
+		},
+	},
+	--   [require("luasnip.util.types").choiceNode] = {
+	--     active = {
+	--       virt_text = { {"•", "GruvboxOrange" } },
+	--     },
+	--   },
+	-- },
 })
-
-
