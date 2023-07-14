08837f3a14b6491de485ec85141a9e2fc63e378a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-07-14 17:50:26 -0400

    feat: Extract math TeX snippets and move to separate file. Clean snippets up.

---
+++ config/nvim/snippets/tex/init.lua
@@ -0,0 +1,194 @@
+-- https://github.com/gillescastel/latex-snippets/blob/master/tex.snippets
+
+local ls = require("luasnip")
+local s = ls.s --> snippet
+local i = ls.i --> insert node
+local t = ls.t --> text node
+local c = ls.choice_node --> choice_node
+local extras = require("luasnip.extras")
+local rep = extras.rep
+-- local fmt = require("luasnip.extras.fmt").fmt --> format node
+local fmta = require("luasnip.extras.fmt").fmta --> format node
+
+-- local conditions = require("luasnip.extras.conditions")
+-- local make_condition = require("luasnip.extras.conditions").make_condition
+local conds_expand = require("luasnip.extras.conditions.expand")
+
+local f = ls.function_node
+
+local autosnippet = ls.extend_decorator.apply(s, { snippetType = "autosnippet" })
+
+local function env(name)
+	-- local is_inside = vim.fn["vimtex#env#is_inside"](name)
+	local is_inside = vim.api.nvim_eval("vimtex#env#is_inside('" .. name .. "')")
+	return (is_inside[1] > 0 and is_inside[2] > 0)
+end
+
+local function in_minted()
+	return env("minted")
+end
+
+local function in_list()
+	return env("enumerate") or env("itemize")
+end
+
+-- local minted = make_condition(in_minted)
+-- local list = make_condition(in_list)
+
+return {
+	-- Environments
+
+	autosnippet(
+		{
+			trig = "mjava",
+			name = "minted java environment",
+		},
+		fmta(
+			[[
+      \begin{minted}{java}
+        <>
+      \end{minted}
+    ]],
+			{ i(1) }
+		),
+		{
+			condition = conds_expand.line_begin,
+		}
+	),
+
+	autosnippet(
+		{
+			trig = "beg",
+			name = "environment",
+		},
+		fmta(
+			[[
+      \begin{<>}
+        <>
+      \end{<>}
+    ]],
+			{ i(1), i(2), rep(1) }
+		),
+		{
+			condition = conds_expand.line_begin,
+		}
+	),
+
+	-- Shortcuts
+
+	-- Formatting
+	autosnippet(
+		{ trig = "1. ", name = "enumerate" },
+		fmta(
+			[[
+    \begin{enumerate}
+      \item <>
+    \end{enumerate}]],
+			{ i(1) }
+		),
+		{ condition = conds_expand.line_begin }
+	),
+
+	autosnippet(
+		{ trig = "- ", name = "itemize" },
+		fmta(
+			[[
+    \begin{itemize}
+      \item <>
+    \end{itemize}]],
+			{ i(1) }
+		),
+		{ condition = conds_expand.line_begin }
+	),
+
+	autosnippet({ trig = "- ", name = "item" }, t("\\item "), {
+		-- condition = list and conds_expand.line_begin,
+	}),
+
+	autosnippet(
+		{ trig = "# ", name = "chapter" },
+		fmta("\\chapter*{<>}", { i(1) }),
+		{ condition = conds_expand.line_begin }
+	),
+
+	autosnippet(
+		{ trig = "## ", name = "section" },
+		fmta("\\section*{<>}", { i(1) }),
+		{ condition = conds_expand.line_begin }
+	),
+
+	autosnippet(
+		{ trig = "### ", name = "subsection" },
+		fmta("\\subsection*{<>}", { i(1) }),
+		{ condition = conds_expand.line_begin }
+	),
+
+	autosnippet(
+		{ trig = "#### ", name = "subsubsection" },
+		fmta("\\subsubsection*{<>}", { i(1) }),
+		{ condition = conds_expand.line_begin }
+	),
+
+	autosnippet({ trig = "tt", name = "type" }, fmta("\\texttt{<>}", { i(1) })),
+	autosnippet({ trig = "__", name = "ital" }, fmta("\\textit{<>}", { i(1) })),
+	-- autosnippet({ trig = "**", name = "bold" }, fmta("\\textbf{<>}", { i(1) }), { condition = not minted }),
+
+	autosnippet({
+		trig = "`([%a])",
+		regTrig = true,
+		name = "code",
+	}, {
+		f(function(_, snip)
+			return "\\texttt{" .. snip.captures[1]
+		end),
+		i(1),
+		t("}"),
+	}),
+
+	-- template
+	autosnippet(
+		{
+			trig = "doctemp",
+			name = "basic template",
+			dscr = "A basic template for LaTeX expecting variables that get passed into inputs",
+			condition = conds_expand.line_begin,
+		},
+		fmta(
+			[[
+    \documentclass[letter,12pt,article,oneside]{memoir}
+
+    \newcommand{\mytitle}{<>}
+    \newcommand{\myauthorname}{<>}
+    \newcommand{\myauthorid}{<>}
+    % The inputs template is one of:
+    % - document
+    % - article
+    % - letter
+    % - math-lecture-notes
+    % - math-homework
+    \newcommand{\myinputstemplate}{<>}
+
+    % Which file to input for basic settings.
+    \input{}
+    \begin{document}
+
+    <>
+
+    \end{document}
+    ]],
+			{
+				i(1, "Title"),
+				i(2, "Moacir P. de Sá Pereira"),
+				i(3, "mpd2149"),
+				c(4, {
+					t("document"),
+					t("article"),
+					t("letter"),
+					t("math-lecture-notes"),
+					t("math-homework"),
+				}),
+				i(5),
+			}
+		)
+	),
+}
---
+++ config/nvim/snippets/tex/math.lua
@@ -0,0 +1,156 @@
+local ls = require("luasnip")
+local s = ls.s --> snippet
+local i = ls.i --> insert node
+local t = ls.t --> text node
+local fmta = require("luasnip.extras.fmt").fmta --> format node
+
+local f = ls.function_node
+
+local autosnippet = ls.extend_decorator.apply(s, { snippetType = "autosnippet" })
+
+local function in_math()
+	return vim.api.nvim_eval("vimtex#syntax#in_mathzone()") == 1
+end
+
+local math = ls.extend_decorator.apply(s, { condition = in_math })
+
+local automath = ls.extend_decorator.apply(s, {
+	snippetType = "autosnippet",
+	condition = in_math,
+})
+
+return {
+	autosnippet({
+		trig = "dm",
+		name = "display math",
+	}, {
+		t({ "\\[", "  " }),
+		i(1),
+		t({ "", "\\]" }),
+	}),
+
+	autosnippet({
+		trig = "ilm",
+		name = "inline math",
+		regTrig = true,
+	}, {
+		t({ "$" }),
+		i(1, "math"),
+		t({ "$" }),
+	}),
+
+	automath("bmat", {
+		t({ "\\begin{bmatrix}", "  " }),
+		i(1, "bmatrix"),
+		t({ "", "\\end{bmatrix}" }),
+	}),
+
+	-- shortcuts
+
+	automath(
+		{ trig = "([%a])sr", regTrig = true },
+		f(function(_, snip)
+			return snip.captures[1] .. "^2"
+		end)
+	),
+
+	automath(
+		{ trig = "([%a])cb", regTrig = true },
+		f(function(_, snip)
+			return snip.captures[1] .. "^3"
+		end)
+	),
+
+	automath({ trig = "([%a%d])td", regTrig = true }, {
+		f(function(_, snip)
+			return snip.captures[1] .. "^{"
+		end),
+		i(1, "power"),
+		t("}"),
+	}),
+
+	automath( -- subscript digit
+		{ trig = "([%a}])(%d)", regTrig = true },
+		f(function(_, snip)
+			return snip.captures[1] .. "_" .. snip.captures[2]
+		end)
+	),
+
+	automath( -- full subscript
+		{ trig = "([%a])(%d%d)", regTrig = true },
+		f(function(_, snip)
+			return snip.captures[1] .. "_{" .. snip.captures[2] .. "}"
+		end)
+	),
+
+	automath(
+		{ trig = "([%a0])vec", regTrig = true },
+		f(function(_, snip)
+			return "\\vec{" .. snip.captures[1] .. "}"
+		end)
+	),
+
+	automath(
+		{ trig = "([eijxyzmb])hat", regTrig = true },
+		f(function(_, snip)
+			return "\\hat{" .. snip.captures[1] .. "}"
+		end)
+	),
+
+	automath(
+		"//",
+		fmta([[\frac{<>}{<>}]], {
+			i(1, "numerator"),
+			i(2, "denominator"),
+		})
+	),
+
+	automath(
+		{ trig = "([^%s$]+)/", regTrig = true },
+		fmta("\\frac{<>}{<>}", {
+			f(function(_, snip)
+				return snip.captures[1]
+			end),
+			i(1, "denominator"),
+		})
+	),
+
+	automath(
+		{ trig = "([+,&])%.%.%.", regTrig = true },
+		f(function(_, snip)
+			local ellipses = "\\dotsb"
+			if snip.captures[1] == "," then
+				ellipses = "\\dotsc"
+			end
+			return snip.captures[1] .. " " .. ellipses .. " " .. snip.captures[1] .. " "
+		end)
+	),
+
+	math(
+		"sum",
+		fmta([[\sum_{<>}^{<>}]], {
+			i(1, "n = 1"),
+			i(2, "\\infty"),
+		})
+	),
+
+	automath("tx", fmta("\\text{<>}\\;", { i(1) })),
+
+	automath("shint", fmta("\\shortintertext{<>}", i(1))),
+
+	automath("nxn", t("n \\times n")),
+
+	-- Typos
+	automath("codt", t("cdot")),
+
+	-- Symbols
+	automath("==", t("&=")),
+
+	automath("!=", t("\\ne")),
+
+	automath("===", t("\\equiv")),
+
+	automath("!>", t("\\mapsto")),
+
+	automath("=>", t("\\implies")),
+}
