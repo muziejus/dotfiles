a5ff44a6323ac489f5778a0498bf742a977a8916
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-10-19 09:30:42 -0400

    feat: Finally fix tikz snippet conditions.

--- config/nvim/snippets/tex/tikz.lua
+++ config/nvim/snippets/tex/tikz.lua
@@ -1,18 +1,13 @@
 local ls = require("luasnip")
 local s = ls.s --> snippet
 local i = ls.i --> insert node
-local t = ls.t --> text node
-local c = ls.choice_node --> choice_node
 local extras = require("luasnip.extras")
 local rep = extras.rep
 -- local fmt = require("luasnip.extras.fmt").fmt --> format node
 local fmta = require("luasnip.extras.fmt").fmta --> format node
 
 -- local conditions = require("luasnip.extras.conditions")
-local make_condition = require("luasnip.extras.conditions").make_condition
 local conds_expand = require("luasnip.extras.conditions.expand")
-
-local f = ls.function_node
 
 local autosnippet = ls.extend_decorator.apply(s, { snippetType = "autosnippet" })
 
@@ -26,10 +21,14 @@ local function in_tikz()
 	return env("tikzpicture")
 end
 
-local autotikz = ls.extend_decorator.apply(s, {
-	snippetType = "autosnippet",
-	condition = in_tikz and conds_expand.line_begin,
-})
+local function start_of_tikz()
+	return in_tikz() and conds_expand.line_begin
+end
+
+-- local autotikzline = ls.extend_decorator.apply(s, {
+-- 	snippetType = "autosnippet",
+-- 	condition = conds_expand.line_begin + in_tikz,
+-- })
 
 return {
 	autosnippet(
@@ -46,19 +45,19 @@ return {
 			condition = conds_expand.line_begin,
 		}
 	),
-	autotikz(
+	autosnippet(
 		{ trig = "no", name = "node" },
 		fmta("\\node[n] (<>) at (<>) {<>};", { i(1), i(2, "0, 0"), rep(1) }),
-		{ condition = conds_expand.line_begin }
+		{ condition = start_of_tikz }
 	),
-	autotikz(
+	autosnippet(
 		{ trig = "nm", name = "math node" },
 		fmta("\\node[n] (<>) at (<>) {$<>$};", { i(1), i(2, "0, 0"), rep(1) }),
-		{ condition = conds_expand.line_begin }
+		{ condition = start_of_tikz }
 	),
-	autotikz(
+	autosnippet(
 		{ trig = "dr", name = "draw arrow" },
 		fmta("\\draw[s] (<>) to (<>);", { i(1), i(2) }),
-		{ condition = conds_expand.line_begin }
+		{ condition = start_of_tikz }
 	),
 }
