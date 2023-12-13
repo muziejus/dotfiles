40d2a84425a11c23abf9af075133ade91bd6b166
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-12-13 03:31:17 -0500

    feat: Add mad telescope candy

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,19 +1,88 @@
 local trouble = require("trouble.providers.telescope")
 
 local telescope = require("telescope")
+local previewers = require("telescope.previewers")
+local actions = require("telescope.actions")
+
+local new_maker = function(filepath, bufnr, opts)
+	opts = opts or {}
+	filepath = vim.fn.expand(filepath)
+	vim.loop.fs_stat(filepath, function(_, stat)
+		if not stat then
+			return
+		end
+		if stat.size > 100000 then
+			return
+		else
+			previewers.buffer_previewer_maker(filepath, bufnr, opts)
+		end
+	end)
+end
 
 telescope.load_extension("bibtex")
 telescope.load_extension("projections")
+telescope.load_extension("ui-select")
+telescope.load_extension("file_browser")
+telescope.load_extension("dap")
 
 telescope.setup({
 	defaults = {
+		buffer_previewer_maker = new_maker,
+		file_ignore_patterns = {
+			"node_modules",
+			"%_files/*.html",
+			"%_cache",
+			".git/",
+			"site_libs",
+			".venv",
+		},
+		layout_strategy = "flex",
+		sorting_strategy = "ascending",
+		layout_config = {
+			prompt_position = "top",
+		},
 		mappings = {
-			i = { ["<c-t>"] = trouble.open_with_trouble },
 			n = { ["<c-t>"] = trouble.open_with_trouble },
+			i = {
+				["<c-t>"] = trouble.open_with_trouble,
+				["<C-u>"] = false,
+				["<C-d>"] = false,
+				["<esc>"] = actions.close,
+				["<c-j>"] = actions.move_selection_next,
+				["<c-k>"] = actions.move_selection_previous,
+			},
+		},
+	},
+	pickers = {
+		find_files = {
+			hidden = false,
+			find_command = {
+				"rg",
+				"--files",
+				"--hidden",
+				"--glob",
+				"!.git/*",
+				"--glob",
+				"!**/.Rproj.user/*",
+				"--glob",
+				"!_site/*",
+				"--glob",
+				"!docs/**/*.html",
+				"-L",
+			},
 		},
 	},
 	load_extensions = { "yank_history", "bibtex" },
 	extensions = {
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown(),
+		},
+		fzf = {
+			fuzzy = true, -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+		},
 		bibtex = {
 			-- Defaults commented out
 			-- depth = 1,
--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,13 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim", 
-    -- tag = "0.1.1",
-    dependencies = { "nvim-lua/plenary.nvim" }
-  },
-  -- dressing is in ui.lua
-  "nvim-telescope/telescope-bibtex.nvim"
+	{
+		"nvim-telescope/telescope.nvim",
+		-- tag = "0.1.1",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	-- dressing is in ui.lua
+	"nvim-telescope/telescope-bibtex.nvim",
+	{ "nvim-telescope/telescope-ui-select.nvim" },
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	{ "nvim-telescope/telescope-dap.nvim", dependencies = { "mfussenegger/nvim-dap" } },
+	{ "nvim-telescope/telescope-file-browser.nvim" },
 }
