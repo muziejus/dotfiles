397c1a21b13f474ed19996c418167ff957354eaa
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-12-12 20:03:31 -0500

    feat: Improve? completion commands.

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,11 +1,6 @@
 local lsp = require("lsp-zero")
 lsp.preset("minimal")
 
-local has_words_before = function()
-	unpack = unpack or table.unpack
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
 local luasnip = require("luasnip")
 
 lsp.set_server_config({
@@ -122,6 +117,13 @@ require("mason-null-ls").setup({
 -- cmp is already configured using lsp-zero's defaults.
 local cmp = require("cmp")
 local cmp_action = require("lsp-zero").cmp_action()
+local lspkind = require("lspkind")
+lspkind.init()
+
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup({
 	snippet = {
@@ -134,9 +136,17 @@ cmp.setup({
 		documentation = cmp.config.window.bordered(),
 	},
 	mapping = {
+		["<C-f>"] = cmp.mapping.scroll_docs(-4), -- cmp_action.luasnip_jump_forward(),
+		["<C-d>"] = cmp.mapping.scroll_docs(4),
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		-- ["<C-n>"] = cmp.mapping(function(fallback)
+		-- 	else
+		-- 		fallback()
+		-- 	end
+		-- end, { "i", "s" }),
+		["<C-a>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
 		-- ["<Esc>"] = cmp.mapping.abort(),
-		["<C-f>"] = cmp_action.luasnip_jump_forward(),
 		["<C-b>"] = cmp_action.luasnip_jump_backward(),
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
@@ -147,7 +157,7 @@ cmp.setup({
 				luasnip.expand()
 			-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
 			-- they way you will only jump inside the snippet region
-			elseif luasnip.expand_or_jumpable() then
+			elseif luasnip.expand_or_locally_jumpable() then
 				luasnip.expand_or_jump()
 			elseif has_words_before() then
 				cmp.complete()
@@ -158,31 +168,55 @@ cmp.setup({
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
-			elseif luasnip.choice_active() then
-				luasnip.change_choice(-1)
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
+			-- elseif luasnip.choice_active() then
+			-- 	luasnip.change_choice(-1)
+			-- elseif luasnip.jumpable(-1) then
+			-- 	luasnip.jump(-1)
 			else
 				fallback()
 			end
 		end, { "i", "s" }),
 	},
 	sources = {
-		{ name = "luasnip", keyword_length = 2 },
-		{ name = "buffer", keyword_length = 5 },
-		{ name = "nvim_lsp_signature_help" },
-		{ name = "cmp_pandoc" },
-		{ name = "calc" },
-		{ name = "nvim_lsp" },
+		{ name = "otter" },
 		{ name = "path" },
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lsp_signature_help" },
+		{ name = "luasnip", keyword_length = 3, max_item_count = 3 },
+		-- { name = "pandoc_references" },
+		{ name = "buffer", keyword_length = 5, max_item_count = 3 },
+		{ name = "spell" },
+		{ name = "treesitter", keyword_length = 5, max_item_count = 3 },
+		{ name = "calc" },
+		{ name = "latex_symbols" },
+		{ name = "emoji" },
 	},
 	formatting = {
-		fileds = { "abbr", "kind", "menu" },
-		format = require("lspkind").cmp_format({
-			mode = "symbol",
-			maxwidth = 50,
-			ellipsis_char = "…",
+		-- fileds = { "abbr", "kind", "menu" },
+		format = lspkind.cmp_format({
+			-- mode = "symbol",
+			-- maxwidth = 50,
+			-- ellipsis_char = "…",
+			with_text = true,
+			menu = {
+				otter = "[🦦]",
+				copilot = "[]",
+				luasnip = "[snip]",
+				nvim_lsp = "[LSP]",
+				buffer = "[buf]",
+				path = "[path]",
+				spell = "[spell]",
+				pandoc_references = "[ref]",
+				tags = "[tag]",
+				treesitter = "[TS]",
+				calc = "[calc]",
+				latex_symbols = "[tex]",
+				emoji = "[emoji]",
+			},
 		}),
+	},
+	view = {
+		entries = "native",
 	},
 })
 
