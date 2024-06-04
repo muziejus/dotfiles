e4c108e74b65eb0bfe22e0c90dbf826d80b09f45
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2024-06-04 09:40:18 -0400

    feat: Change git signs to use regular text symbols, not bars.

--- config/nvim/after/plugin/git.lua
+++ config/nvim/after/plugin/git.lua
@@ -17,31 +17,36 @@ require("gitsigns").setup({
 	},
 	attach_to_untracked = true,
 	signs = {
-		add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-		change = {
-			hl = "GitSignsChange",
-			text = "▎",
-			numhl = "GitSignsChangeNr",
-			linehl = "GitSignsChangeLn",
-		},
-		delete = {
-			hl = "GitSignsDelete",
-			text = "",
-			numhl = "GitSignsDeleteNr",
-			linehl = "GitSignsDeleteLn",
-		},
-		topdelete = {
-			hl = "GitSignsDelete",
-			text = "",
-			numhl = "GitSignsDeleteNr",
-			linehl = "GitSignsDeleteLn",
-		},
-		changedelete = {
-			hl = "GitSignsChange",
-			text = "▎",
-			numhl = "GitSignsChangeNr",
-			linehl = "GitSignsChangeLn",
-		},
+		add = { text = "+" },
+		change = { text = "~" },
+		delete = { text = "_" },
+		topdelete = { text = "‾" },
+		changedelete = { text = "~" },
+		-- add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+		-- change = {
+		-- 	hl = "GitSignsChange",
+		-- 	text = "▎",
+		-- 	numhl = "GitSignsChangeNr",
+		-- 	linehl = "GitSignsChangeLn",
+		-- },
+		-- delete = {
+		-- 	hl = "GitSignsDelete",
+		-- 	text = "",
+		-- 	numhl = "GitSignsDeleteNr",
+		-- 	linehl = "GitSignsDeleteLn",
+		-- },
+		-- topdelete = {
+		-- 	hl = "GitSignsDelete",
+		-- 	text = "",
+		-- 	numhl = "GitSignsDeleteNr",
+		-- 	linehl = "GitSignsDeleteLn",
+		-- },
+		-- changedelete = {
+		-- 	hl = "GitSignsChange",
+		-- 	text = "▎",
+		-- 	numhl = "GitSignsChangeNr",
+		-- 	linehl = "GitSignsChangeLn",
+		-- },
 	},
 	on_attach = function(bufnr)
 		local gs = package.loaded.gitsigns
