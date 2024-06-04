a66f17a41dcd811564a5da485aaef9c6f0f8fc1a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2024-06-04 09:29:40 -0400

    feat: switch to default, new neovim colorscheme.

--- config/nvim/after/plugin/colors.lua
+++ config/nvim/after/plugin/colors.lua
@@ -5,16 +5,15 @@ function ColorMyPencils(color)
 	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-  require("barbecue").setup {
-    theme = color,
-  }
+	require("barbecue").setup({
+		theme = color,
+	})
 
-  require("lualine").setup { 
-    options = {
-      theme = color,
-    },
-  }
-
+	require("lualine").setup({
+		options = {
+			theme = color,
+		},
+	})
 end
 
-ColorMyPencils()
+-- ColorMyPencils()
