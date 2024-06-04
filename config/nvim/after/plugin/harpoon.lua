a2712930ca6e7aadf394fd195fc34737a2bfe505
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2024-06-04 10:42:35 -0400

    feat: Cleanup harpoon 2 maybe now it'll work.

--- config/nvim/after/plugin/harpoon.lua
+++ config/nvim/after/plugin/harpoon.lua
@@ -1,26 +1,41 @@
 local harpoon = require("harpoon")
 
 -- REQUIRED
-harpoon:setup({})
+harpoon:setup({
+	settings = {
+		save_on_toggle = false,
+		sync_on_ui_close = false,
+		save_on_change = true,
+		enter_on_sendcmd = false,
+		excluded_filetyupes = { "harpoon", "alpha", "dashboard", "gitcommit" },
+		mark_branch = false,
+		key = function()
+			return vim.loop.cwd()
+		end,
+	},
+})
 -- REQUIRED
 
 -- Keymaps in keymaps.
 
+-- For a keybinding to use the telescope fuzzy finder
 -- local conf = require("telescope.config").values
 -- local function toggle_telescope(harpoon_files)
---     local file_paths = {}
---     for _, item in ipairs(harpoon_files.items) do
---         table.insert(file_paths, item.value)
---     end
+-- 	local file_paths = {}
+-- 	for _, item in ipairs(harpoon_files.items) do
+-- 		table.insert(file_paths, item.value)
+-- 	end
 --
---     require("telescope.pickers").new({}, {
---         prompt_title = "Harpoon",
---         finder = require("telescope.finders").new_table({
---             results = file_paths,
---         }),
---         previewer = conf.file_previewer({}),
---         sorter = conf.generic_sorter({}),
---     }):find()
+-- 	require("telescope.pickers")
+-- 		.new({}, {
+-- 			prompt_title = "Harpoon",
+-- 			finder = require("telescope.finders").new_table({
+-- 				results = file_paths,
+-- 			}),
+-- 			previewer = conf.file_previewer({}),
+-- 			sorter = conf.generic_sorter({}),
+-- 		})
+-- 		:find()
 -- end
 --
 -- vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -1,1 +1,6 @@
-return { "ThePrimeagen/harpoon", branch = "harpoon2", dependencies = { "nvim-lua/plenary.nvim" } }
+return {
+	"ThePrimeagen/harpoon",
+	enabled = true,
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+}
