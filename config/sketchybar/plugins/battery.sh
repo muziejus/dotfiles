d73dd5d5ed484ed8acce88509ff524ca1c09d139
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2025-03-19 04:30:01 -0400

    feat: Do some SketchyBar stuff.

--- config/sketchybar/plugins/battery.sh
+++ config/sketchybar/plugins/battery.sh
@@ -25,4 +25,5 @@ fi
 
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
+# sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
+sketchybar --set "$NAME" icon="$ICON"
