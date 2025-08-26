c34a58e94593204638506b2e0c623bc666534bbd
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2025-08-26 10:24:45 -0400

    feat: Add Snacks.

---
+++ config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,10 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		bigfile = { enabled = true },
+		bufdelete = { enabled = true },
+		dashboard = { example = "files" },
+	},
+}
