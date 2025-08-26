8e8c4a007ee7c6bca88fb7d39c08419cef825214
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2025-08-26 10:25:09 -0400

    feat: Autoformat whatever.

--- config/nvim/ftplugin/markdown.lua
+++ config/nvim/ftplugin/markdown.lua
@@ -1,6 +1,6 @@
 -- It's funny. This is set to 0 in set.lua just so markdown doesn't
 -- conceal, but, like, now I want concealing?
-vim.opt["conceallevel"] = 2
+-- vim.opt["conceallevel"] = 2
 
 vim.opt.spelllang = "en_us"
 vim.opt.spell = true
