f466d30f4cec78d486853d329d238e6009527328
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2026-02-24 22:19:32 -0500

    feat: Use smarter regex ventilator.

--- config/nvim/ftplugin/markdown.lua
+++ config/nvim/ftplugin/markdown.lua
@@ -5,38 +5,50 @@
 vim.opt.spelllang = "en_us"
 vim.opt.spell = true
 
--- function OneSentencePerLine()
---   local mode = vim.api.nvim_get_mode()["mode"]
---   if string.match(mode, "[iR]") then
---     return
---   end
---   local start = vim.api.nvim_win_get_cursor(0)[1]
---   local grafStart = vim.v.lnum
---   local grafEnd = vim.v.count - 1
---
---   local lines = vim.iter()
---   lines:join("{}")
---
--- end
--- vim.opt.formatexpr = OneSentencePerLine() --
--- require("../lua/one-sentence-per-line")
-
 vim.api.nvim_command("call pencil#init()")
 
-vim.cmd([[
-    function! OneSentencePerLine()
-    if mode() =~# '^[iR]'
-      return
-    endif
-    let start = v:lnum
-    let end = start + v:count - 1
-    execute start.','.end.'join'
-    " s/[.!?]['’”].\zs\s*\ze\S/\r/g
-    s/\([.,!?][”’'"]*\)\s\+/\1\r/g
-    " let start = v:lnum
-    " let end = start + v:count - 1
-    " execute start.','.end.'join'
-    " s/^\([a-z]\{1}\)/  \1/g
-    endfunction
-    set formatexpr=OneSentencePerLine()
-  ]])
+vim.bo.formatexpr = "v:lua.MarkdownVentilate()"
+
+function _G.MarkdownVentilate()
+	if vim.fn.mode():match("^[iR]") then
+		return 0
+	end
+
+	local start = vim.v.lnum
+	local finish = start + vim.v.count - 1
+
+	-- Trim trailing blank lines from range
+	while finish > start and vim.fn.getline(finish):match("^%s*$") do
+		finish = finish - 1
+	end
+
+	-- Treesitter paragraph check
+	local parser = vim.treesitter.get_parser(0, "markdown")
+	if not parser then
+		return 0
+	end
+
+	local tree = parser:parse()[1]
+	local root = tree:root()
+	local row = start - 1
+	local node = root:named_descendant_for_range(row, 0, row, 0)
+
+	while node do
+		if node:type() == "paragraph" then
+			break
+		end
+		node = node:parent()
+	end
+
+	if not node or node:type() ~= "paragraph" then
+		return 0
+	end
+
+	-- Join paragraph only (no blank line)
+	vim.cmd(string.format("%d,%djoin", start, finish))
+
+	-- Split sentences
+	vim.cmd(string.format("%ds/\\([.!?][\"'”’]*\\)\\s\\+/\\1\\r/g", start))
+
+	return 0
+end
